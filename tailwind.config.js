a09b9ddc8a2f5a09077eb3ea188adfc005db6803
Wojciech Wieclawski <wojtekwieclawski@gmail.com>
2022-01-05 21:12:28 +0100

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
-//tailwind.config.js
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html", "./src/components/*.{js,jsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       height: {
         "screen-js": "calc(100 * var(--vh))",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
